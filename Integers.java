61db0ff955bdcaa4b199bca666dd495af6bfe8c5
yodaad <yodaad@gmail.com>
2021-06-06 18:20:34 -0500

    4 archivos cargados

---
+++ Integers.java
@@ -0,0 +1,21 @@
+
+public class Integers {
+
+	public static void main(String[] args) {
+		
+		int a=4;
+		int b=2;
+		int c=0;
+		int d=999999999; //Integers have a number limit. To have more numbers use a long
+		int e=-700; 
+		
+		System.out.println(a+" plus "+b +" is: "+(a+b));      //Add Operator is +
+		System.out.println(a+" minus "+b +" is: "+(a-b));   //Substract Operator is -
+		System.out.println(a+" times "+b +" is: "+(a*b));   //Multiply operator is *
+		System.out.println(a+" divided into "+b +" is: "+(a/b));   //Divide Operator is /
+		System.out.println(a+" MOD  "+b +" is: "+(a%b)); //MOD Operator is %
+	
+
+	}
+
+}
